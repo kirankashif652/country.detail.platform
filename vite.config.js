d2e0a75241402b866a7ae16ae82f7d3ab044abf1
kirankashif <kirankashif933@gmail.com>
2025-07-17 17:06:47 +0500

    react project

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/country.detail.platform/" : "/",
+  base:"/country.detail.platform/",
 
 })
