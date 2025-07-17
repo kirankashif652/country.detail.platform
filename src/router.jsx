3900f236da4a9c091bb314cd56d5dce2de631b1c
kirankashif <kirankashif933@gmail.com>
2025-07-17 17:17:34 +0500

    react project

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,21 @@
-import { createBrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
 import Home from './pages/Home';
 import CountryDetails from './pages/CountryDetails';
 import NotFound from './pages/NotFound';
 
-const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/country/:name', element: <CountryDetails /> },
-  { path: '*', element: <NotFound /> }
-]);
-
-export default router;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="country/:name" element={<CountryDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/country.detail.platform/",
-
+  base: '/country.detail.platform/',
 })
